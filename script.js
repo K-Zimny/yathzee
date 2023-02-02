ac08b89c7629f4212776e3279499d9f7cba4d79b
Kenny Zimny <kenny.zimny@criticalmass.com>
2023-02-02 12:05:25 -0600

    Added sum logic for entire game

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 "use strict"
 //----------------------------------------------------------------------------------------------//
+
+// upper section
+
 let onesBox = document.getElementById("ones");
 let onesArray = [1,2,3,4,5,0];
 let onesArrayPosition = 0;
@@ -32,31 +35,37 @@ let sixesArrayPosition = 0;
 onesBox.addEventListener("click", function(){
     onesArrayPosition = plusOneArray(onesBox, onesArray, onesArrayPosition);
     addUpperScore();
+    totalGameScore()
 });
 
 twosBox.addEventListener("click", function(){
     twosArrayPosition = plusOneArray(twosBox, twosArray, twosArrayPosition);
     addUpperScore();
+    totalGameScore()
 });
 
 threesBox.addEventListener("click", function(){
     threesArrayPosition = plusOneArray(threesBox, threesArray, threesArrayPosition);
     addUpperScore();
+    totalGameScore()
 });
 
 foursBox.addEventListener("click", function(){
     foursArrayPosition = plusOneArray(foursBox, foursArray, foursArrayPosition);
     addUpperScore();
+    totalGameScore()
 });
 
 fivesBox.addEventListener("click", function(){
     fivesArrayPosition = plusOneArray(fivesBox, fivesArray, fivesArrayPosition);
     addUpperScore();
+    totalGameScore()
 });
 
 sixesBox.addEventListener("click", function(){
     sixesArrayPosition = plusOneArray(sixesBox, sixesArray, sixesArrayPosition);
     addUpperScore();
+    totalGameScore()
 });
 
 function plusOneArray(box, array, arrayPosition) {
@@ -74,7 +83,98 @@ let topScoreDisplay = document.getElementById("topScoreDisplay")
 
 function addUpperScore(){
     topScoreDisplay.innerHTML = parseInt(onesBox.innerHTML) + parseInt(twosBox.innerHTML) + parseInt(threesBox.innerHTML) + parseInt(foursBox.innerHTML) + parseInt(fivesBox.innerHTML) + parseInt(sixesBox.innerHTML)
+    if(topScoreDisplay.innerHTML >= 63) {
+        topScoreDisplay.innerHTML = parseInt(topScoreDisplay.innerHTML) +  35
+    }
 }
 
 //----------------------------------------------------------------------------------------------//
+
+// Lower Section 
+
+const threeOfKind = document.getElementById("threeOfKind");
+threeOfKind.addEventListener("click", function(){
+    threeOfKind.select();
+})
+threeOfKind.addEventListener("input", function(){
+    addLowerScore();
+    totalGameScore()
+})
+
+const fourOfKind = document.getElementById("fourOfKind");
+fourOfKind.addEventListener("click", function(){
+    fourOfKind.select();
+})
+fourOfKind.addEventListener("input", function(){
+    addLowerScore();
+})
+
+const fullHouse = document.getElementById("fullHouse");
+fullHouse.addEventListener("click", function(){
+    displayScore(fullHouse, 25)
+    addLowerScore();
+    totalGameScore()
+})
+
+const smStraight = document.getElementById("smStraight");
+smStraight.addEventListener("click", function(){
+    displayScore(smStraight, 30)
+    addLowerScore();
+    totalGameScore()
+})
+
+const lgStraight = document.getElementById("lgStraight");
+lgStraight.addEventListener("click", function(){
+    displayScore(lgStraight, 40)
+    addLowerScore();
+    totalGameScore()
+})
+
+const yahtzee = document.getElementById("yahtzee");
+yahtzee.addEventListener("click", function(){
+    displayScore(yahtzee, 50)
+    addLowerScore();
+    totalGameScore()
+})
+
+const yahtzeeBonus = document.getElementById("yahtzeeBonus");
+yahtzeeBonus.addEventListener("click", function(){
+    displayScore(yahtzeeBonus, 100)
+    addLowerScore();
+    totalGameScore()
+})
+
+const chance = document.getElementById("chance");
+chance.addEventListener("click", function(){
+    chance.select();
+})
+chance.addEventListener("input", function(){
+    addLowerScore();
+    totalGameScore()
+})
+
+function displayScore(display, scoreValue){
+    if (display.innerHTML == 0) {
+        display.innerHTML = scoreValue
+    } else {
+        display.innerHTML = 0;
+    }
+}
+
+//add lower scores and display as total 
+
+let bottomScoreDisplay = document.getElementById("bottomScoreDisplay");
+
+function addLowerScore() {
+    bottomScoreDisplay.innerHTML = parseInt(threeOfKind.value) + parseInt(fourOfKind.value) + parseInt(fullHouse.innerHTML) + parseInt(smStraight.innerHTML) + parseInt(lgStraight.innerHTML) + parseInt(yahtzee.innerHTML) + parseInt(yahtzeeBonus.innerHTML) + parseInt(chance.value)
+}
+
+//----------------------------------------------------------------------------------------------//
+
+//total game score
+
+const totalGameScoreDisplay = document.getElementById("totalGameScore")
+function totalGameScore() {
+    totalGameScoreDisplay.innerHTML = parseInt(bottomScoreDisplay.innerHTML) + parseInt(topScoreDisplay.innerHTML);
+}
 
