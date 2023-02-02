148fb01f567129a90bce5f0d778360d0171ef340
Kenny Zimny <kenny.zimny@criticalmass.com>
2023-02-02 10:59:46 -0600

    Wrote plusOneArray function and added sum for upperscore function

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+"use strict"
+//----------------------------------------------------------------------------------------------//
 let onesBox = document.getElementById("ones");
 let onesArray = [1,2,3,4,5,0];
 let onesArrayPosition = 0;
@@ -22,16 +24,57 @@ let sixesBox = document.getElementById("sixes");
 let sixesArray = [6,12,18,24,30,0];
 let sixesArrayPosition = 0;
 
-//----------------------------------------------------------------------------------------------//
+//-------------------------------------------//
 
-//on click, move 1 space in the array
+//on click, move 1 space through the array. 
+//add all the scores for a upper section total
 
 onesBox.addEventListener("click", function(){
-    plusOneArray(onesBox, onesArray, onesArrayPosition)
+    onesArrayPosition = plusOneArray(onesBox, onesArray, onesArrayPosition);
+    addUpperScore();
+});
+
+twosBox.addEventListener("click", function(){
+    twosArrayPosition = plusOneArray(twosBox, twosArray, twosArrayPosition);
+    addUpperScore();
+});
+
+threesBox.addEventListener("click", function(){
+    threesArrayPosition = plusOneArray(threesBox, threesArray, threesArrayPosition);
+    addUpperScore();
+});
+
+foursBox.addEventListener("click", function(){
+    foursArrayPosition = plusOneArray(foursBox, foursArray, foursArrayPosition);
+    addUpperScore();
+});
+
+fivesBox.addEventListener("click", function(){
+    fivesArrayPosition = plusOneArray(fivesBox, fivesArray, fivesArrayPosition);
+    addUpperScore();
+});
+
+sixesBox.addEventListener("click", function(){
+    sixesArrayPosition = plusOneArray(sixesBox, sixesArray, sixesArrayPosition);
+    addUpperScore();
 });
 
 function plusOneArray(box, array, arrayPosition) {
     box.innerHTML = array[arrayPosition]
     arrayPosition++;
+    if(arrayPosition > array.length - 1 ) {
+        arrayPosition = 0;
+    }
     return arrayPosition;
 }
+
+//add upper scores and display as total
+
+let topScoreDisplay = document.getElementById("topScoreDisplay")
+
+function addUpperScore(){
+    topScoreDisplay.innerHTML = parseInt(onesBox.innerHTML) + parseInt(twosBox.innerHTML) + parseInt(threesBox.innerHTML) + parseInt(foursBox.innerHTML) + parseInt(fivesBox.innerHTML) + parseInt(sixesBox.innerHTML)
+}
+
+//----------------------------------------------------------------------------------------------//
+
